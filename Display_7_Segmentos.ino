b2e563a63d4b34b5030b0eb4a7f13da4bad5356e
Adriano dos Santos Lisboa <robertopereira23447@gmail.com>
2021-10-16 16:49:51 -0300

    Ligar display de 7 segmentos

---
+++ Display_7_Segmentos.ino
@@ -0,0 +1,37 @@
+int a=2;
+int b=3;
+int c=4;
+int d=5;
+int e=6;
+int f=7;
+int g=8;
+
+
+void setup ()
+{
+    pinMode (a, OUTPUT);
+    pinMode (b, OUTPUT);
+    pinMode (c, OUTPUT);
+    pinMode (d, OUTPUT);
+    pinMode (e, OUTPUT);
+    pinMode (f, OUTPUT);
+    pinMode (g, OUTPUT);
+}
+
+void loop ()
+{
+    digitalWrite (a, HIGH);
+    delay (1000);
+    digitalWrite (b, HIGH);
+    delay (1000);
+    digitalWrite (c, HIGH);
+    delay (1000);
+    digitalWrite (d, HIGH);
+    delay (1000);
+    digitalWrite (e, HIGH);
+    delay (1000);
+    digitalWrite (f, HIGH);
+    delay (1000);
+    digitalWrite (g, HIGH);
+    delay (1000);
+}
